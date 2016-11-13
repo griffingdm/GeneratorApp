b23fa23276e1fdfc18c94839381e092c2fcc0cf1
Griffin Mullins <griffin.mullins@capitalone.com>
2016-11-13 16:02:26 -0600

    Major game updates!! lookin good
    this is a good stopping point for now I think.

--- GeneratorApp/GameViewController.swift
+++ GeneratorApp/GameViewController.swift
@@ -19,15 +19,14 @@ class GameViewController: UIViewController {
     @IBOutlet weak var surgerThree: UIView!
     @IBOutlet weak var surgerFour: UIView!
     @IBOutlet weak var surgerFive: UIView!
+    @IBOutlet weak var surgesLabel: UILabel!
     
-    @IBOutlet weak var dotShot: FullRoundView!
-    @IBOutlet weak var dotShotTwo: FullRoundView!
-    @IBOutlet weak var dotShotthree: FullRoundView!
-    @IBOutlet weak var dotShotFour: FullRoundView!
-    @IBOutlet weak var dotShotFive: FullRoundView!
-    
+    @IBOutlet var theDots: [FullRoundView]!
+    @IBOutlet var scoreIndicators: [FullRoundView]!
+    @IBOutlet var cannons: [FullRoundView]!
     
     @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var logoImageBackground: UIImageView!
     
     var surgers: [UIView]!
     var dotShots: [FullRoundView]!
@@ -56,26 +55,203 @@ class GameViewController: UIViewController {
         
         surgers = [surgerOne, surgerTwo, surgerThree, surgerFour, surgerFive]
         
-        dotShots = [dotShot, dotShotTwo, dotShotthree, dotShotFour, dotShotFive]
-        
-        for dot in dotShots {
+        for dot in theDots {
             dot.alpha = 0
         }
         
+        surgesLabel.transform = surgesLabel.transform.rotated(by: toRadians(degrees: 270))
+        view.layoutIfNeeded()
+        
         //view.bringSubview(toFront: logoImageView)
+        //animateCircuit()
+    }
+    
+    override func viewDidLayoutSubviews() {
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        animateCircuit()
+    }
+    
+    func animateCircuit(){
+        logoImageBackground.layer.removeAllAnimations()
+        
+        logoImageBackground.alpha = 0
+        
+        UIView.animate(withDuration: 2, delay: 0, options: [.repeat, .autoreverse], animations: {
+            self.logoImageBackground.alpha = 1
+        }){(finished: Bool) -> Void in
+        }
+    }
+    
+    func stopCircuit(){
+        self.logoImageBackground.alpha = 1
+        UIView.animate(withDuration: 1, delay: 0, options: [], animations: {
+            self.logoImageBackground.alpha = 0
+        }){(finished: Bool) -> Void in
+            self.logoImageBackground.layer.removeAllAnimations()
+        }
     }
     
     @IBAction func gunDown(_ sender: UITapGestureRecognizer) {
         let gun: UITapGestureRecognizer = sender
         scene.shoot(tag:  (gun.view?.tag)!)
         
-        var theDot = dotShots[(gun.view?.tag)!]
-        theDot.alpha = 1
-        UIView.animate(withDuration: 0.25, animations:{
-            theDot.alpha = 0
-        })
+        if scene.view?.isPaused == false {
+            for dot in theDots{
+                if dot.tag == gun.view?.tag{
+                    dot.alpha = 1
+                    
+                    UIView.animate(withDuration: 0.25, animations:{
+                        dot.alpha = 0
+                    })
+                }
+            }
+            
+            cannon()
+        }
     }
     
+    func toRadians(degrees: Double) -> CGFloat{
+        return (CGFloat(degrees * M_PI) / 180.0)
+    }
+    
+    func indicateScore(score: Int){
+        for indicator in scoreIndicators {
+            switch score {
+            case 10:
+                if indicator.tag == 0 {
+                    foreverIndicate(theView: indicator)
+                }
+            case 20:
+                if indicator.tag == 1 {
+                    foreverIndicate(theView: indicator)
+                }
+            case 30:
+                if indicator.tag == 2 {
+                    foreverIndicate(theView: indicator)
+                }
+            case 40:
+                if indicator.tag == 3 {
+                    foreverIndicate(theView: indicator)
+                }
+            case 50:
+                if indicator.tag == 4 {
+                    foreverIndicate(theView: indicator)
+                }
+            case 100:
+                allYellow()
+            default:
+                return
+            }
+        }
+    }
+    
+    func foreverIndicate(theView: UIView){
+        UIView.animate(withDuration: 0.5, delay: 0, options: [.repeat, .autoreverse], animations: {
+            theView.backgroundColor = #colorLiteral(red: 0.9254901961, green: 0.8745098039, blue: 0.7333333333, alpha: 1)
+        }){(finished: Bool) -> Void in
+        }
+    }
+    
+    func allYellow(){
+        for indicator in scoreIndicators{
+            indicator.layer.removeAllAnimations()
+            UIView.animate(withDuration: 0.5, delay: 0, options: [], animations: {
+                indicator.backgroundColor = #colorLiteral(red: 0.9254901961, green: 0.8745098039, blue: 0.7333333333, alpha: 1)
+            }){(finished: Bool) -> Void in
+            }
+        }
+    }
+    
+    func killIndicators(score: Int){
+        let upperTag: Int = Int(floor(CGFloat(score)/10.0)) - 1
+        
+        for indicator in scoreIndicators {
+            if indicator.tag <= upperTag {
+                indicator.layer.removeAllAnimations()
+                UIView.animate(withDuration: 0.5, delay: 0, options: [], animations: {
+                    indicator.backgroundColor = #colorLiteral(red: 0.9254901961, green: 0.8745098039, blue: 0.7333333333, alpha: 1)
+                }){(finished: Bool) -> Void in
+                    UIView.animate(withDuration: 1, delay: 2, options: [], animations: {
+                        indicator.backgroundColor = #colorLiteral(red: 0.1176470588, green: 0.168627451, blue: 0.2, alpha: 1)
+                    }){(finished: Bool) -> Void in
+                    }
+                }
+            }
+        }
+    }
+    
+    func flashSurger(surger: UIView){
+        UIView.animate(withDuration: 0.1, delay: 0, options: [.repeat, .autoreverse], animations: {
+            UIView.setAnimationRepeatCount(15)
+            surger.backgroundColor = #colorLiteral(red: 0.9176470588, green: 0.2588235294, blue: 0.1450980392, alpha: 1)
+        }){(finished: Bool) -> Void in
+        }
+        
+        stopCircuit()
+    }
+    
+    func returnSurger(rSurger: UIView){
+        rSurger.layer.removeAllAnimations()
+        UIView.animate(withDuration: 0.5, animations: {
+            rSurger.backgroundColor = #colorLiteral(red: 0.9176470588, green: 0.2588235294, blue: 0.1450980392, alpha: 1)
+        }){(finished: Bool) -> Void in
+            UIView.animate(withDuration: 1, delay: 0.5, options: [], animations: {
+                rSurger.backgroundColor = #colorLiteral(red: 0.1176470588, green: 0.168627451, blue: 0.2, alpha: 1)
+            }){(finished: Bool) -> Void in
+            }
+        }
+    }
+    
+    func flashShooter(theDotTag: Int){
+        for dot in theDots {
+            if dot.tag == theDotTag{
+                dot.backgroundColor = #colorLiteral(red: 0.9176470588, green: 0.2588235294, blue: 0.1450980392, alpha: 1)
+                UIView.animate(withDuration: 0.1, delay: 0, options: [.repeat, .autoreverse], animations: {
+                    UIView.setAnimationRepeatCount(15)
+                    dot.alpha = 1
+                }){(finished: Bool) -> Void in
+                }
+            }
+        }
+        
+        stopCircuit()
+    }
+    
+    func cannon(){
+        for cannon in cannons {
+            cannon.layer.removeAllAnimations()
+            
+            let delayAmount = Double(cannon.tag) / 20.0
+            
+            UIView.animate(withDuration: 0, delay: delayAmount, options: [], animations: {
+                cannon.backgroundColor = #colorLiteral(red: 0.9254901961, green: 0.8745098039, blue: 0.7333333333, alpha: 1)
+            }){(finished: Bool) -> Void in
+                UIView.animate(withDuration: 0.2, delay: 0, options: [], animations: {
+                    cannon.backgroundColor = #colorLiteral(red: 0.1176470588, green: 0.168627451, blue: 0.2, alpha: 1)
+                }){(finished: Bool) -> Void in
+                }
+            }
+        }
+    }
+    
+    func returnShooter(theDotTag: Int){
+        for dot in theDots {
+            if dot.tag == theDotTag{
+                UIView.animate(withDuration: 0.5, delay: 0, options: [], animations: {
+                    dot.alpha = 1
+                    dot.backgroundColor = #colorLiteral(red: 0.9176470588, green: 0.2588235294, blue: 0.1450980392, alpha: 1)
+                }){(finished: Bool) -> Void in
+                    UIView.animate(withDuration: 1, delay: 0.5, options: [], animations: {
+                        dot.alpha = 0
+                    }){(finished: Bool) -> Void in
+                        dot.backgroundColor = #colorLiteral(red: 0.9254901961, green: 0.8745098039, blue: 0.7333333333, alpha: 1)
+                    }
+                }
+            }
+        }
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -100,7 +276,7 @@ class GameViewController: UIViewController {
         if win == true {
             destController.resultString = "You've Won!"
         } else {
-            destController.resultString = "It overloaded! Try Again."
+            destController.resultString = "SYSTEM OVERLOAD!"
         }
     }
     
