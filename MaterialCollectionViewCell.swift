8ad7c2e7df3cfdb68e98977b77024f8e0e6add96
Griffin Mullins <griffin.mullins@capitalone.com>
2016-11-20 22:01:43 -0600

    reference materials on server, copy images out of assets

--- MaterialCollectionViewCell.swift
+++ MaterialCollectionViewCell.swift
@@ -12,9 +12,19 @@ class MaterialCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var materialImage: UIImageView!
     @IBOutlet weak var materialLabel: UILabel!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    var imageLink: String?
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+    }
+    
+    func loadImage(){
+        self.activityIndicator.startAnimating()
+        materialImage.downloadedFrom(link: imageLink!, completion: {
+            self.activityIndicator.stopAnimating()
+        })
     }
 }
